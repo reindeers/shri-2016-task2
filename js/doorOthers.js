ff07b9a432b3c4df561acf2e1bc0b8ed2c3e900c
reindeers <velokarri@mail.ru>
2016-07-10 22:30:57 +0300

    Update doorOthers.js

--- js/doorOthers.js
+++ js/doorOthers.js
@@ -35,11 +35,11 @@ function Door0(number, onUnlock) {
      */
     function checkCondition() {
         var isOpened = true;
-        buttons.forEach(function(b) {
+      /*  buttons.forEach(function(b) {
             if (!b.classList.contains('door-riddle__button_pressed')) {
                 isOpened = false;
             }
-        });
+        });*/
 
         // Если все три кнопки зажаты одновременно, то откроем эту дверь
         if (isOpened) {
@@ -111,13 +111,79 @@ Door1.prototype.constructor = DoorBase;
 function Door2(number, onUnlock) {
     DoorBase.apply(this, arguments);
 
-    // ==== Напишите свой код для открытия третей двери здесь ====
-    // Для примера дверь откроется просто по клику на неё
-    this.popup.addEventListener('click', function() {
-        this.unlock();
-    }.bind(this));
-    // ==== END Напишите свой код для открытия третей двери здесь ====
+    var button = this.popup.querySelector(".door-riddle__button_2");
+    var canvas = this.popup.querySelector(".door-riddle__canvas");
+    button.addEventListener('dblclick', _dblclick.bind(this)); //todo: point!
+    function _dblclick(e) {
+      //  World.add(engine.world, [Bodies.circle(200, 200, 10)]);
+      var Engine = Matter.Engine,
+          Render = Matter.Render,
+          World = Matter.World,
+          Body = Matter.Body,
+          Composites = Matter.Composites,
+          Bodies = Matter.Bodies,
+          Mouse = Matter.Mouse,
+          MouseConstraint = Matter.MouseConstraint,
+          Constraint = Matter.Constraint;
+
+      // create an engine
+      var engine = Engine.create();
+
+      // create a renderer
+      var render = Render.create({
+          element: canvas,
+          engine: engine
+      });
+
+      mouseConstraint = MouseConstraint.create(engine, {
+          element: render.canvas
+      });
+
+              // pass mouse to renderer to enable showMousePosition
+            //  demo.render.mouse = demo.mouseConstraint.mouse;
+
+      // create two boxes and a ground
+      var target = Bodies.circle(200, 200, 10);
+      var ball = Bodies.circle(350, 150, 20, { isStatic: true, density: 0.1 });
+      var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+      var group = Body.nextGroup(true),
+                  particleOptions = { friction: 0.00001, collisionFilter: { group: group }, render: { visible: false }, density: 1},
+                  cloth = Composites.softBody(200, 200, 10, 2, 5, 5, false, 8, particleOptions);
+                  cloth.bodies[0].isStatic = true;
+                  cloth.bodies[9].isStatic = true;
+
+
+      /*var elastic = Constraint.create({
+                      pointA: cloth,
+                      bodyB: target,
+                      stiffness: 0.05,
+                      render: {
+                          lineWidth: 5,
+                          strokeStyle: '#dfa417'
+                      }});*/
+
+
+      // add all of the bodies to the world
+      World.add(engine.world, mouseConstraint);
+      World.add(engine.world, [target, ball, cloth, ground]);
+
+      // run the engine
+      Engine.run(engine);
+
+      // run the renderer
+      Render.run(render);
+    };
+
+
+    document.addEventListener('pointerdown', _onButtonPointerDown.bind(this));
+
+    function _onButtonPointerDown(e) {
+
+    };
+    //console.log(target.position.x==ball.position.x && target.position.y==ball.position.y);
 }
+
 Door2.prototype = Object.create(DoorBase.prototype);
 Door2.prototype.constructor = DoorBase;
 
@@ -141,7 +207,7 @@ function Box(number, onUnlock) {
 
     block.addEventListener('pointermove', _onButtonPointerMove.bind(this));
     block.addEventListener('pointerleave', _onButtonPointerLeave.bind(this));
-    block.addEventListener('pointerup', _onButtonPointerUp.bind(this));
+  //  block.addEventListener('pointerup', _onButtonPointerUp.bind(this));
 
     function _onButtonPointerMove(e) {
         checkCondition.apply(this);
@@ -154,7 +220,7 @@ function Box(number, onUnlock) {
 
 
     function checkCondition() {
-      if ((event.clientY > block.getBoundingClientRect().top
+      if ((event.clientY < block.getBoundingClientRect().top
         && event.clientY < block.getBoundingClientRect().bottom)
         && event.clientX > block.getBoundingClientRect().right) {
           this.unlock();
