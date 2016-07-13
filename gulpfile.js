98812d7c84a447971b7446dfdcb6d3bab6faedd1
reindeers <velokarri@mail.ru>
2016-07-14 00:24:34 +0300

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('dev', function(callback) {
-    sequence('build:examples', 'watch', 'serve', callback);
-});
